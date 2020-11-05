33b036e622d45f7834e715d9ed1f68966b44af59
beritcarvalho <56369664+beritcarvalho@users.noreply.github.com>
2020-11-04 23:22:33 -0300

    Encapsulamento dos itens da nota
    mudanca de cor do botao add
    encapsulamento das variaveis das notas

---
+++ BloquinhodeNotas/app/src/main/java/com/example/bloquinhodenotas/pojo/Nota.java
@@ -0,0 +1,42 @@
+package com.example.bloquinhodenotas.pojo;
+
+import java.util.Date;
+
+public class Nota {
+    private int idNota;
+    private String titulo;
+    private String texto;
+    private Date dataCriacao;
+
+    public int getIdNota() {
+        return idNota;
+    }
+
+    public void setIdNota(int idNota) {
+        this.idNota = idNota;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+
+    public Date getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(Date dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+}
